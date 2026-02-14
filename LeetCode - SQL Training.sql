bb9096c814adb7aed4564f823de995772b57bf20
Amir <96538080+AmirShala@users.noreply.github.com>
2026-02-14 13:01:36 +0200

    Add files via upload

---
+++ LeetCode - SQL Training.sql
@@ -0,0 +1,215 @@
+--Crack SQL Interview in 50 Qs
+
+--EASY questions
+
+/*Q1: Find Customer Referee
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Find the names of the customer that are either:
+
+referred by any customer with id != 2.
+not referred by any customer.
+
+Solution:
+
+select name
+from customer
+where referee_id!=2 or referee_id is  null
+
+=================================================================================================================================
+
+Q2: Big Countries
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+
+A country is big if:
+
+it has an area of at least three million (i.e., 3000000 km2), or
+it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+
+Solution:
+
+select name, population, area
+from World
+where area >= 3000000 or population >= 25000000
+
+=================================================================================================================================
+
+Q3: Article Views I
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table, the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+ 
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+
+Return the result table sorted by id in ascending order.
+
+Solution:
+
+select distinct author_id as id 
+from Views
+where author_id = viewer_id
+order by id 
+
+=================================================================================================================================
+
+Q4: Invalid Tweets
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| tweet_id       | int     |
+| content        | varchar |
++----------------+---------+
+tweet_id is the primary key (column with unique values) for this table.
+content consists of alphanumeric characters, '!', or ' ' and no other special characters.
+This table contains all the tweets in a social media app.
+ 
+
+Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
+
+Solution:
+
+select tweet_id
+from Tweets
+where len(content) > 15
+
+=================================================================================================================================
+
+Q5: Replace Employee ID With The Unique Identifier
+
+Table: Employees
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return the result table in any order.
+
+Solution:
+
+select Unique_id, name 
+from Employees E 
+LEFT JOIN EmployeeUNI E2 ON E.id = E2.id
+
+=================================================================================================================================
+
+Q6: Product Sales Analysis I
+
+Table: Sales
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+
+Return the resulting table in any order.
+
+
+Solution:
+
+select product_name, year, price
+from sales s 
+join product p on s.product_id = p.product_id
+
+
+=================================================================================================================================
+
+Q7: Rising Temperature
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+ 
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+Solution:
+
+with Ranked as (
+
+    select * ,
+    lag (temperature) over (order by recordDate) as PreviousTemp,
+    lag (recordDate ) over (order by recordDate) as PreviousDate
+    from weather
+)
+select id
+from Ranked
+where PreviousTemp < temperature and abs(datediff(day,recordDate,PreviousDate)) < 2
+
+*/
+
